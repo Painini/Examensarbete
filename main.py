b48f00b4c8e927c5a3ba6492615fe94f6ff08dba
Painini <tim.lofstedt@elev.ga.ntig.se>
2023-05-11 13:17:53 +0200

    feat: added images

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import cv2
 from skimage.metrics import structural_similarity as ssim
 
-originalImg = cv2.imread("Original Image here") #Load Unchanged PNG file here
-upscaledImg = cv2.imread("Upscaled Image here") #Load Upscaled JPG file here
+originalImg = cv2.imread('Images/A.png') #Load Unchanged PNG file here
+upscaledImg = cv2.imread('Images/AJPGimgupscaler.jpg') #Load Upscaled JPG file here
 
 score, diff = ssim(originalImg, upscaledImg, full=True)
 
