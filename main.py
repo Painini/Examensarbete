6ca895e4d790a8807dbd0ec3187f47723beec6fb
Painini <tim.lofstedt@elev.ga.ntig.se>
2023-05-12 13:11:11 +0200

    misc: added and removed images

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import cv2
 from skimage.metrics import structural_similarity as ssim
 
-originalImg = cv2.imread('Images/A.png') #Load Unchanged PNG file here
-upscaledImg = cv2.imread('Images/AJPGimgupscalerResized.png') #Load Upscaled JPG file here
+originalImg = cv2.imread('Images/A.jpg') #Load Unchanged PNG file here
+upscaledImg = cv2.imread('Images/ACompressedImgupscalerResized.jpg') #Load Upscaled JPG file here
 
 score = ssim(originalImg, upscaledImg, channel_axis= -1)
 
